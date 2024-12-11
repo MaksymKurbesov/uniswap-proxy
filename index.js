82d50fe4adfc0ae8477f8decfa039679452ff1de
MaksymKurbesov <bonyklade@gmail.com>
2024-12-11 15:30:32 +0200

    added post route

--- index.js
+++ index.js
@@ -64,5 +64,18 @@ app.get('/proxy', (req, res) => {
   );
 });
 
+app.post('/proxy', (req, res) => {
+  request(
+    { url: `${API_URL}` },
+    (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        return res.status(500).json({ type: 'error', message: error.message });
+      }
+
+      res.json(JSON.parse(body));
+    }
+  );
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
