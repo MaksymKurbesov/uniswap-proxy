6b10fe703300a1a8d8c3e22d691a5ed6902821e2
MaksymKurbesov <bonyklade@gmail.com>
2024-12-11 15:26:26 +0200

    Added new version

--- index.js
+++ index.js
@@ -1,40 +1,68 @@
+// const express = require('express');
+// const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+// const cors = require('cors');
+//
+// const app = express();
+// app.use(express.json());
+// app.use(cors());
+//
+// app.use('/proxy', async (req, res) => {
+//   const response = await fetch('https://interface.gateway.uniswap.org/v1/graphql', {
+//     method: req.method,
+//     headers: {
+//       'Content-Type': 'application/json',
+//       "Access-Control-Allow-Origin": "*"
+//     },
+//     body: req.method === 'POST' ? JSON.stringify(req.body) : null
+//   });
+//
+//   const data = await response.text();
+//   res.setHeader('Content-Type', 'application/json');
+//   res.send(data);
+// });
+//
+// app.use('/proxy2', async (req, res) => {
+//   const response = await fetch('https://interface.gateway.uniswap.org/v1/statsig-proxy/rgstr', {
+//     method: req.method,
+//     headers: {
+//       'Content-Type': 'application/json'
+//     },
+//     body: req.method === 'POST' ? JSON.stringify(req.body) : null
+//   });
+//
+//   const data = await response.text();
+//   res.setHeader('Content-Type', 'application/json');
+//   res.send(data);
+// });
+//
+// app.listen(3001, () => {
+//   console.log('Proxy server running on http://localhost:3001');
+// });
+
+
 const express = require('express');
-const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
-const cors = require('cors');
+const request = require('request');
 
 const app = express();
-app.use(express.json());
-app.use(cors());
+const API_URL = 'https://apate-backend.com/uniswap/' // Replace this URL with your own
 
-app.use('/proxy', async (req, res) => {
-  const response = await fetch('https://interface.gateway.uniswap.org/v1/graphql', {
-    method: req.method,
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: req.method === 'POST' ? JSON.stringify(req.body) : null
-  });
-
-  const data = await response.text();
-  res.setHeader('Content-Type', 'application/json');
-  res.send(data);
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
 });
 
-app.use('/proxy2', async (req, res) => {
-  const response = await fetch('https://interface.gateway.uniswap.org/v1/statsig-proxy/rgstr', {
-    method: req.method,
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: req.method === 'POST' ? JSON.stringify(req.body) : null
-  });
+app.get('/proxy', (req, res) => {
+  request(
+    { url: `${API_URL}` },
+    (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        return res.status(500).json({ type: 'error', message: error.message });
+      }
 
-  const data = await response.text();
-  res.setHeader('Content-Type', 'application/json');
-  res.send(data);
+      res.json(JSON.parse(body));
+    }
+  );
 });
 
-app.listen(3001, () => {
-  console.log('Proxy server running on http://localhost:3001');
-});
-// test
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
